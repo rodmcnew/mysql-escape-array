6de771cf1c2d0543d768efbe26753e26b07cc140
rodmcnew <rodmcnew@gmail.com>
2016-05-17 15:39:20 -0500

    update

--- test/humanTest.js
+++ test/humanTest.js
@@ -5,5 +5,4 @@ var names = ["bob","joe","bill","Robert' DROP TABLE Students;"];
 var sql = 'SELECT * FROM users WHERE name IN ' + mysqlEscapeArray(names);
 
 console.log(sql);
-
 //SELECT * FROM users WHERE name IN ('bob','joe','bill','Robert\' DROP TABLE Students;')
