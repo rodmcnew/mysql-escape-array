a8ccb57c5fcaef1f39985148afca08c90b2e7562
rodmcnew <rodmcnew@gmail.com>
2016-05-17 15:32:10 -0500

    add files

--- test/humanTest.js
+++ test/humanTest.js
@@ -1,9 +1,5 @@
-# mysql-escape-array
-Escape arrays before putting them in mysql queries like 'where id in (3421, 6789, 6754)
-
-```js
-var mysqlEscapeArray = require('mysql-escape-array');
+var mysqlEscapeArray = require('../index.js');
 var names = ["bob","joe","bill","Robert' DROP TABLE Students;"];
 var sql = 'select * from users where name in ' + mysqlEscapeArray(names);
-console.log(sql)
+console.log(sql);
 //Returns: select * from users where name in ('bob','joe','bill','Robert\' DROP TABLE Students;')
